b1812492352e39b85a74dfb13cff0f2f738410f3
Skiesuwu <joshwyag@gmail.com>
2023-05-11 12:55:28 -0700

    whoops

--- src/api/Status/status.ts
+++ src/api/Status/status.ts
@@ -1,10 +1,10 @@
-import express from "express";
+import { Router } from "express";
 import { API_BASE } from "../../config/config.json";
 import statusConfig, { name, status, message } from "../../config/status.json";
 import { ServerStatus } from "../../utils/Status/StatusInterface";
-const app = express.Router();
+const app = Router();
 
-app.get(`${API_BASE}/status`, (req, res) => {
+app.get(`${API_BASE}status`, (req, res) => {
   const status: ServerStatus = {
     name,
     status: statusConfig.status,
