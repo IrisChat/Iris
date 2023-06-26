331d51c4ae6a78bd682b69d466cdf7216131d2c4
AlexDev404 <alexdev404@user.noreply.github.com>
2023-06-26 17:07:09 -0600

    Uploading is screwed again

--- src/socket/WebSocket.ts
+++ src/socket/WebSocket.ts
@@ -3,7 +3,6 @@ import { API_BASE } from "../config/config.json";
 import User from "../Database/models/User";
 import Room from "../Database/models/Room";
 import { Warn, Error, Gateway } from "../utils";
-import Axios, { AxiosError } from "axios";
 import { filetransfer_key } from "../config/config.json";
 let userMessageCache: any[] = [];
 let onlineUsers: any = {};
@@ -312,210 +311,92 @@ function ws_main(io: any) {
             "server-message",
             JSON.stringify(serverMsg(1, "RENDERING..."))
           );
-          type type2 = {
-            type: number;
-            content: Buffer;
-            description: string;
-            size: number;
-            filename: string;
-            mimetype: string;
-            ts: number;
-          };
 
-          console.log("[FILETRANSFER.IO] STARTING FILE TRANSFER...");
+          console.log("[FILE_TRANSFER::NOSTR] STARTING FILE TRANSFER...");
           // data = JSON.parse(data); // The data is already parsed - we dont need to do this
 
           // The server only accepts "Uint8Array"s since it will textDecode after uploaded
-          const contentBuffer = Uint8Array.from(
+          const contentBuffer = new Uint8Array(
             [...data.content].map((ch) => ch.charCodeAt())
           ).buffer;
+
           const fileContent = contentBuffer;
           // break;
-          let server_host: string;
-          let server_protocol: string;
-          // Skeleton object
-          type fdata_skeleton = {
-            uploadUrl: string;
-            uploadToken: string;
-            maxBytes: Number;
-            success: Boolean;
-            ownerDetailLink: string;
-            state: any[];
-            snippets: {
-              "snippet--upload-progress": string;
-              "snippet--header": string;
-              "snippet--footer": string;
-            };
-          };
-          let fdata: fdata_skeleton;
-          // Create new session
-          Axios({
-            method: "GET",
-            url: "https://filetransfer.io/start-upload",
 
-            headers: {
-              "X-Requested-With": "XMLHttpRequest",
-              cookie: filetransfer_key,
-            },
-
-            withCredentials: true,
-            responseType: "json",
+          const file = new FormData();
+          file.append("fileToUpload", Buffer.from(fileContent));
+          console.log(file);
+          // Upload the file
+          fetch("https://nostr.build/api/upload/iris.php", {
+            method: "POST",
+            body: file,
           })
             .then((response) => {
               // console.log(response);
               // @ts-ignore
-              fdata = response.data;
-              console.log(fdata.uploadUrl);
-              server_host = new URL(fdata.uploadUrl).hostname;
-              server_protocol = new URL(fdata.uploadUrl).protocol;
+
+              console.log(response);
 
               if (response.status != 200) {
-                Error("[FILETRANSFER.IO] SERVER REQUEST FAILED");
+                Error("[FILE_TRANSFER::NOSTR] SERVER REQUEST FAILED");
                 socket.emit(
                   "server-message",
                   JSON.stringify(serverMsg(1, "THIRD-PARTY REQUEST FAILED"))
                 );
                 return;
               }
-              console.log(
-                data.size,
-                `filename ${Buffer.from(data.filename).toString(
-                  "base64"
-                )},filetype ${Buffer.from(data.mimetype).toString(
-                  "base64"
-                )},fileorder MQ==`
-              ); // Strictly no spaces whatsoever
-              // Reserve the session
-              Axios({
-                method: "POST",
-                url: fdata.uploadUrl,
-                headers: {
-                  "Upload-Length": data.size,
-                  "Upload-Metadata": `filename ${Buffer.from(
-                    data.filename
-                  ).toString("base64")},filetype ${Buffer.from(
-                    data.mimetype
-                  ).toString("base64")},fileorder MQ==`, // Strictly no spaces whatsoever
-                  "Tus-Resumable": "1.0.0",
-                },
-              }).then((response) => {
-                console.log("The session has been reserved.");
-                // Upload the data
 
-                if (response.status != 201) {
-                  Error("[FILETRANSFER.IO] SERVER REQUEST FAILED");
-                  socket.emit(
-                    "server-message",
-                    JSON.stringify(serverMsg(1, "COULD NOT RESERVE A SESSION"))
-                  );
-                  return;
-                }
-                // @ts-ignore
-                const chunk_host = response.headers.get("Location");
-                console.log(`${server_protocol}//${server_host}${chunk_host}`);
-                console.log(
-                  `${server_protocol}//${server_host}${chunk_host}`,
-                  data.size
-                );
+              const deliveryLink: string = response.toString();
 
-                Axios({
-                  url: `${server_protocol}//${server_host}${chunk_host}`,
-                  // @ts-ignore
-
-                  headers: {
-                    "Content-Length": data.size,
-                    "Content-Type": "application/offset+octet-stream",
-                    "Upload-Offset": "0",
-                    "Tus-Resumable": "1.0.0",
-                  },
-                  method: "PATCH",
-                  data: fileContent,
-                })
-                  .then((response) => {
-                    if (response.status != 200) {
-                      Error("[FILETRANSFER.IO] CHUNK UPLOAD FAILED");
-                      socket.emit(
-                        "server-message",
-                        JSON.stringify(serverMsg(1, "CHUNK UPLOAD FAILED"))
-                      );
-                      return;
-                    }
-                  })
-                  .then(() => {
-                    console.log(
-                      `https://filetransfer.io/finish-delivery/${fdata.uploadToken}`
-                    );
-                    Axios({
-                      method: "POST",
-                      url: `https://filetransfer.io/finish-delivery/${fdata.uploadToken}`,
-                      headers: {
-                        "X-Requested-With": "XMLHttpRequest",
-                      },
-                      responseType: "json",
-                    })
-                      .then((res) => {
-                        console.log(res.data); // @ts-ignore
-                        const deliveryLink =
-                          res.data.deliveryPublicLink.split("#")[0];
-                        if (/image.*/.test(data.mimetype)) {
-                          messageAsMe(
-                            `<img src="${deliveryLink + "/download"}" />`
-                          );
-                        } else if (/video.*/.test(data.mimetype)) {
-                          messageAsMe(`
+              if (/image.*/.test(data.mimetype)) {
+                messageAsMe(`<img src="${deliveryLink + "/download"}" />`);
+              } else if (/video.*/.test(data.mimetype)) {
+                messageAsMe(`
                         <video controls>
                            <source src="${deliveryLink + "/download"}" type=${
-                            data.mimetype
-                          }>
+                  data.mimetype
+                }>
                           Your browser does not support the HTML5 video element
                         </video>
                         `);
-                        } else {
-                          messageAsMe(
-                            `<a href="${
-                              deliveryLink + "/download"
-                            }">${deliveryLink}</a>`
-                          );
-                        }
+              } else {
+                messageAsMe(
+                  `<a href="${deliveryLink + "/download"}">${deliveryLink}</a>`
+                );
+              }
 
-                        function messageAsMe(content: any) {
-                          socket.emit(
-                            "message",
-                            JSON.stringify(
-                              userMsg(data.IAM, content, data.description)
-                            )
-                          );
+              function messageAsMe(content: any) {
+                socket.emit(
+                  "message",
+                  JSON.stringify(userMsg(data.IAM, content, data.description))
+                );
 
-                          socket
-                            .to(roomID)
-                            .emit(
-                              "message",
-                              JSON.stringify(
-                                userMsg(data.IAM, content, data.description)
-                              )
-                            );
+                socket
+                  .to(roomID)
+                  .emit(
+                    "message",
+                    JSON.stringify(userMsg(data.IAM, content, data.description))
+                  );
 
-                          // Append the message everytime
-                          userMessageCache[roomID].push(
-                            userMsg(data.IAM, content, data.description)
-                          );
-                        }
-                        console.log(`Successfully uploaded ${data.filename}`);
-                      })
-
-                      .catch((error) => {
-                        const err = error as AxiosError;
-                        if (err.response) {
-                          console.log(err.response.status);
-                          console.log(err.request);
-                          console.log(err.response.data);
-                        } else {
-                          // console.log(err);
-                        }
-                      });
-                  });
-              });
+                // Append the message everytime
+                userMessageCache[roomID].push(
+                  userMsg(data.IAM, content, data.description)
+                );
+              }
+              console.log(`Successfully uploaded ${data.filename}`);
             })
+
+            .catch((error) => {
+              const err = error as AxiosError;
+              if (err.response) {
+                console.log(err.response.status);
+                console.log(err.request);
+                console.log(err.response.data);
+              } else {
+                console.log(err);
+              }
+            })
+
             .catch((error) => {
               const err = error as AxiosError;
               if (err.response) {
